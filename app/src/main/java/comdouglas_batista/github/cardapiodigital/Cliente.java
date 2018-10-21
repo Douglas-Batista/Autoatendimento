c876e5c55f2413178115d83830ab990f360f24be
Douglas <douglasbmacedo@gmail.com>
2018-10-21 20:09:03 -0300

    adicionar prato, pronto

--- app/src/main/java/comdouglas_batista/github/cardapiodigital/Cliente.java
+++ app/src/main/java/comdouglas_batista/github/cardapiodigital/Cliente.java
@@ -8,20 +8,11 @@ import android.widget.Button;
 
 public class Cliente extends Activity {
 
-    private Button exemplo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_cliente);
 
-        exemplo = (Button) findViewById(R.id.btnExemplo);
 
-        exemplo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent entPrato = new Intent(Cliente.this, Prato.class);
-                startActivity(entPrato);
-            }
-        });
     }
 }
