ae6d1dd440ae52bd692f01453ba150c8a0bfc20b
Douglas <douglasbmacedo@gmail.com>
2018-10-21 20:53:30 -0300

    tela adicionar prato concluida

--- app/src/main/java/comdouglas_batista/github/cardapiodigital/GerenciarPratos.java
+++ app/src/main/java/comdouglas_batista/github/cardapiodigital/GerenciarPratos.java
@@ -1,13 +1,28 @@
 package comdouglas_batista.github.cardapiodigital;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GerenciarPratos extends Activity {
+
+    private Button atualizar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gerenciar_pratos);
+
+        atualizar = (Button) findViewById(R.id.btnAtua);
+
+        atualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent entPrato = new Intent(GerenciarPratos.this, AtualizarExcluir.class);
+                startActivity(entPrato);
+            }
+        });
     }
 }
