0f1d575dc1ed928934435edc6975bf8fd9231359
Douglas <douglasbmacedo@gmail.com>
2018-10-22 01:29:44 -0300

    adicionar prato, pronto

--- app/src/main/java/comdouglas_batista/github/cardapiodigital/AdicionarPrato.java
+++ app/src/main/java/comdouglas_batista/github/cardapiodigital/AdicionarPrato.java
@@ -4,14 +4,26 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
 
 public class AdicionarPrato extends Activity {
 
     private static int IMAGEM_PEGA = 1;
     private ImageView imagem;
+    EditText numero, nome, ingrediente,preco;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +32,46 @@ public class AdicionarPrato extends Activity {
 
         imagem = (ImageView) findViewById(R.id.imagemPrato);
         Button pegaimagem = (Button) findViewById(R.id.SelImagem);
+        numero = (EditText) findViewById(R.id.numPrato);
+        nome = (EditText) findViewById(R.id.nomePrato);
+        ingrediente = (EditText) findViewById(R.id.ingredientesPrato);
+        preco = (EditText) findViewById(R.id.precoPrato);
+        Button addPrato = (Button) findViewById(R.id.btnAddPrato);
 
+        iniciarFirebase();
+
+        addPrato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Modelo prato = new Modelo();
+                prato.setNumeroPrato(numero.getText().toString());
+                prato.setNomePrato(nome.getText().toString());
+                prato.setPrecoPrato(preco.getText().toString());
+                prato.setIngrdientesPrato(ingrediente.getText().toString());
+                //prato.setImagemPrato(imagem.getImage().toString());
+
+                if(numero.getText().toString().isEmpty()||nome.getText().toString().isEmpty()||
+                        preco.getText().toString().isEmpty()||ingrediente.getText().toString().isEmpty()){
+
+                    Toast.makeText(getApplicationContext(),"Preencha todos os campos",Toast.LENGTH_LONG).show();
+
+                } else {
+
+                    databaseReference.child("Prato").child(prato.getNumeroPrato()).setValue(prato);
+                    limparCampos();
+                }
+            }
+
+            private void limparCampos() {
+                numero.setText("");
+                nome.setText("");
+                preco.setText("");
+                ingrediente.setText("");
+            }
+        });
+
+
+        //pega imagem na galeria
         pegaimagem.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -34,6 +85,8 @@ public class AdicionarPrato extends Activity {
             }
         });
     }
+
+    //coloca a imagem no image view
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode== IMAGEM_PEGA  && resultCode == RESULT_OK) {
@@ -41,5 +94,12 @@ public class AdicionarPrato extends Activity {
             imagem.setImageURI(fullPhotoUri);
         }
     }
+
+    private void iniciarFirebase() {
+        FirebaseApp.initializeApp(AdicionarPrato.this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+    }
+
 }
 
